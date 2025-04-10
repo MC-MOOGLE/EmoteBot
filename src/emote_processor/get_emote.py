d7a3aa6dcdb3dcf21e301fabe9747713c4d7987b
MC-MOOGLE <dmitriybosoy@gmail.com>
2025-04-10 13:02:22 +0300

    get emote

--- src/emote_processor/get_emote.py
+++ src/emote_processor/get_emote.py
@@ -1,2 +1,39 @@
-def get_emote():
+import cv2
+from deepface import DeepFace
+
+def get_emote(image_path: str):
+    """Detect human emotion in an image.
+    
+    Args:
+        image_path: Path to the image file
+        
+    Returns:
+        str: Dominant detected emotion or None if no human found
+        
+    Raises:
+        FileNotFoundError: If image file doesn't exist
+        ValueError: If image is corrupted
+    """
+    try:
+        analysis = DeepFace.analyze(image_path, actions=['emotion'], silent=True)
+
+        print(type(analysis), analysis)
+
+        if not analysis or not isinstance(analysis, list):
+            raise ValueError("No face detected or unable to recognize emotion in the image.")
+
+        if len(analysis) > 1:
+            raise ValueError("Multiple faces detected")
+
+        result = analysis[0]
+        
+        dominant_emotion = result['dominant_emotion']
+        print(f"Dominant emotion: {dominant_emotion}")
+        return dominant_emotion
+    except FileNotFoundError:
+        raise FileNotFoundError
+    
     return None
+
+if __name__ == "__main__":
+    get_emote("test_images/sad_1.jpg")
--- tests/emote_processor/test_get_emote.py
+++ tests/emote_processor/test_get_emote.py
@@ -1,4 +1,13 @@
+import pytest
 from src.emote_processor.get_emote import get_emote
 
 def test_get_emote_no_file():
-    assert get_emote() is None
+    with pytest.raises(ValueError):
+        get_emote("test_images/sadd_1.jpg")
+
+def test_get_emote():
+    result = get_emote("test_images/sad_2.jpg")
+
+    print(result)
+
+    assert result == "angry"
