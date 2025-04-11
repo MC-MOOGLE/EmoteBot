a98af03c964d69db10c8f01e13c620c4668427fa
MC-MOOGLE <dmitriybosoy@gmail.com>
2025-04-11 11:17:07 +0300

    opencv benchmark

--- src/emote_processor/get_emote.py
+++ src/emote_processor/get_emote.py
@@ -25,7 +25,9 @@ def get_emotions(image_path: str, backend: str = 'retinaface'):
             silent=True
         )
 
-        print(type(analysis), analysis)
+        emotes = analysis[0].get('emotion')
+        if emotes:
+            print(*(f"{key}: {int(value * 10) / 10}" for key, value in emotes.items()), sep=", ")
 
         if not analysis or not isinstance(analysis, list):
             raise ValueError("No face detected or unable to recognize emotion in the image.")
@@ -45,12 +47,14 @@ def get_emotions(image_path: str, backend: str = 'retinaface'):
     return None
 
 if __name__ == "__main__":
-    for backend in ('opencv', 'ssd', 'mtcnn', 'retinaface'):
-        result = get_emotions("test_images/sad_1.jpg", backend)
+    for image in ("angry_1", "angry_2", "happy_1", "happy_2", "sad_1", "sad_2"):
+        print(f"======= {image} =========")
+        for backend in ('opencv', 'ssd', 'mtcnn', 'retinaface'):
+            result = get_emotions(f"test_images/{image}.jpg", backend)
 
-        if not result:
-            print(f"{backend}: no emotion detected")
-            continue
-        
-        dominant_emotion = result['dominant_emotion']
-        print(f"{backend}: {dominant_emotion}")
+            if not result:
+                print(f"{backend}: no emotion detected\n")
+                continue
+
+            dominant_emotion = result['dominant_emotion']
+            print(f"{backend}: {dominant_emotion}\n")
